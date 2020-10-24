b595b3bf0960578a6ef16cbbc072965e213f724b
maribelgr <mgrageraruiz@gmail.com>
2020-10-24 17:55:51 +0200

    Add js list item functions

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ const button = document.querySelector(".js-button");
 const results = document.querySelector(".js-results");
 
 let series = [];
+let favorites = [];
 
 //Acceder a la URL para obtener los resultados de la búsqueda
 function getResults() {
@@ -12,32 +13,48 @@ function getResults() {
   fetch(`//api.tvmaze.com/search/shows?q=${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
-      //Dar valor al array de series --> series = data.series; ver si es así o series = data;
-      //Llamar aquí a la función que muestre los resultados showResults
       series = data;
-      // console.log(series);
-      showResults();
-      // for (const character of data.results) {
-      //   result.innerHTML += `<li>Tu personaje es ${character.name} y su género es ${character.gender}</li>`;
+      //Llamo ahora a la función que muestra el resultado de la búsqueda = los li
+      paintResults();
+      //console.log(series);
+      //Lamo a la función que ha añadido un evento click a cada li
+      listenAddItems();
     });
 }
 
-//Crear funcion que muestre resultados
-function showResults() {
+//Crear funcion que muestre resultados de la búsqueda -que pinte los li-
+function paintResults() {
   let result = "";
-  for (const serie of series) {
-    result += "<li>";
+  for (let i = 0; i < series.length; i++) {
+    const serie = series[i];
+    const src = serie.show.image
+      ? `${serie.show.image.medium}`
+      : "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+    result += `<li class="list__results-item" data-id="${serie.show.id}">`;
     result += `<div class="list__results-container">`;
-    result += `<img class="list__results-img" src="${serie.show.image.medium}" alt="${serie.show.name}">`;
+    result += `<img class="list__results-img" src="${src}" alt="${serie.show.name}">`;
     result += `<h2 class="list__results-title">${serie.show.name}</h2>`;
     result += "</div>";
     result += "</li>";
   }
-  // console.log(result);
+  console.log(result);
   results.innerHTML = result;
 }
 
-button.addEventListener("click", getResults);
+//Creo función que añada los listener a los li recorriendo cada uno de ellos. A esta función la tengo que llamar dentro del 2º then, (como a la funcion de pintar los li) porque es cuando se genera la búsqueda y se pintan los resultados
+const listenAddItems = () => {
+  const items = document.querySelectorAll(".list__results-item");
+  //Creo un for para recorrer cada elemento li y poder añadirle el evento click
+  for (const item of items) {
+    //Le pongo función para que le añada clase de favoritos al item (li) que he clicado
+    item.addEventListener("click", addFavoriteItem);
+  }
+};
 
-//con showResults voy a pintar los li a los que he accedido
-//ahora quiero coger todos los li que he pintado en el html con el showResults (y Después del console log de pintar quiero coger
+//Crear función para añadir a favoritos: AQUÍ QUIERO IDENTIFICAR EL ELEMENTO CLICADO
+function addFavoriteItem(event) {
+  // const itemId = event.currentTarget.id;
+  console.log(event.currentTarget.dataset.id);
+}
+
+button.addEventListener("click", getResults);
