47f4a17b8bd55b42114a777d5802e2a7bd4bb425
maribelgr <mgrageraruiz@gmail.com>
2020-10-26 14:01:36 +0100

    Add styles

--- src/js/main.js
+++ src/js/main.js
@@ -8,22 +8,19 @@ const favouritesList = document.querySelector(".js-favourites");
 let series = [];
 let favourites = [];
 
-//Acceder a la URL para obtener los resultados de la búsqueda
 function getResults() {
   let inputValue = input.value;
   fetch(`//api.tvmaze.com/search/shows?q=${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
       series = data;
-      //Llamo ahora a la función que muestra el resultado de la búsqueda = los li
+
       paintResults();
-      //Lamo a la función que ha añadido un evento click a cada li
       listenAddItems();
       paintFavourites();
     });
 }
 
-//Crear funcion que muestre resultados de la búsqueda -que pinte los li-
 function paintResults() {
   let result = "";
   for (let i = 0; i < series.length; i++) {
@@ -58,13 +55,9 @@ const listenAddItems = () => {
 };
 
 function addFavoriteItem(event) {
-  //Esto es para añadirle la clase con color al background
   const clickedSerie = event.currentTarget;
-  //Obtengo el Id de la serie
   const clickedId = event.currentTarget.id;
-  //Busco si el id del array de la búsqueda -series- es igual que el id del elemento clicado
   const object = series.find((serie) => serie.show.id === parseInt(clickedId));
-  //Busco el index de la serie para saber si está en favoritos o no // Situar el id en el index
   const serieIndex = favourites.findIndex(
     (serie) => serie.show.id === parseInt(clickedId)
   );
@@ -79,7 +72,6 @@ function addFavoriteItem(event) {
   }
 }
 
-//Mostrar listado de favoritos
 function paintFavourites() {
   let favouritesResult = "";
   const resetBtn = document.querySelector(".js-reset-btn");
@@ -95,7 +87,7 @@ function paintFavourites() {
     favouritesResult += "</div>";
     favouritesResult += "</li>";
   }
-  // console.log(favouritesResult);
+
   favouritesList.innerHTML = favouritesResult;
   if (favourites.length === 0) {
     resetBtn.classList.add("reset-button-hidden");
@@ -108,42 +100,32 @@ function paintFavourites() {
 const listenFavouriteItems = () => {
   const favouriteItems = document.querySelectorAll(".favourites__list-item");
   const resetBtn = document.querySelector(".js-reset-btn");
-  //Creo un for para recorrer cada elemento li y poder añadirle el evento click
   for (const favouriteItem of favouriteItems) {
-    //Le pongo función para que quite de favoritos al item (li) que he clicado
     favouriteItem.addEventListener("click", removeFavouriteItem);
   }
-  //En este botón añado el evento para borrar todos los favoritos
   resetBtn.addEventListener("click", removeAllFavorites);
 };
 
 function removeAllFavorites() {
-  //Reinicio a 0 el listado de favoritos
   favourites = [];
-  //Borro el localStorage
   localStorage.removeItem("favourites");
-  //Borro en el html el listado que había creado antes
+
   favouritesList.innerHTML = "";
   paintFavourites();
 }
 
-//Crear función para quitar de favoritos: AQUÍ QUIERO IDENTIFICAR EL ELEMENTO CLICADO
 function removeFavouriteItem(event) {
-  //Obtengo el Id de la serie
   const clickedId = event.currentTarget.id;
-  //Busco el index de la serie para saber si está en favoritos o no
+
   const serieIndex = favourites.findIndex(
     (serie) => serie.show.id === parseInt(clickedId)
   );
 
   favourites.splice(serieIndex, 1);
-  //quitar clase del background de los elementos que no son favoritos
-  //añadir clase del background favorito
   paintFavourites();
   setInLocalStorage();
 }
 
-//Recuperar lo que he guardado en favoritos al recargar la página
 const getFromLocalStorage = () => {
   const LocalStoragefavourites = localStorage.getItem("favourites");
   if (LocalStoragefavourites !== null) {
@@ -151,7 +133,6 @@ const getFromLocalStorage = () => {
   }
 };
 
-//Guardo favourites en el local storage, pasándolo antes a string
 const setInLocalStorage = () => {
   const stringifyfavourites = JSON.stringify(favourites);
   localStorage.setItem("favourites", stringifyfavourites);
